c3790ad3ead21e4515aa2273df7076ed4ea23750
Howard Do <howarddo2208@gmail.com>
2023-04-20 19:14:35 +0700

    add more alias for tmux and cheatsheet

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,7 +6,9 @@ alias g='lazygit'
 alias zshrc="nvim $ZDOTDIR"
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 alias nvimrc='nvim ~/.config/nvim/'
-alias txi='tmuxifier'
+alias txi='tmuxifier' # tmux session preset
+alias tmuxrc='nvim ~/.tmux.conf'
+alias cs="cht.sh" # cheatsheet for command lines
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
